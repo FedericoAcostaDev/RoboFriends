ad4a25de6dc6e19fca9a2a2b3a97207e16323288
FedericoAcostaDev <fedeacos045@gmail.com>
2021-06-19 18:25:08 -0300

    error boundry added

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
     console.log(props);
     return (
-        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
         {props.children}
         </div>
     )
